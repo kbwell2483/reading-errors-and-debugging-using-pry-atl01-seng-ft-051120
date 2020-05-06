63af600b3b4bd55895804e82283513388e30d915
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 20:45:23 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,11 @@
 def plus_two(num)
 	num + 2
+<<<<<<< HEAD
 end
 
+=======
+	binding.pry
+end
+
+num(3)
+>>>>>>> ae6f27ac7d2d5f5596ca760c768565d14ab5c47b
--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,6 +1,9 @@
 require 'pry'
+<<<<<<< HEAD
 
 require './lib/pry_debugging.rb'
+=======
+>>>>>>> ae6f27ac7d2d5f5596ca760c768565d14ab5c47b
 
 describe "#plus_two" do 
 	it "takes in a number as an argument and returns the sum of that number and 2" do
